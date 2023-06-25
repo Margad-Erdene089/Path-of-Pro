321f016e70bf8ec3e6525e44db9ff485c6a75438
Margad-Erdene089 <b.margaderdene1231@gmail.com>
2023-06-25 23:11:39 +0800

    operator

---
+++ C/ArithmeticOperators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10, b = 5;
+    int sum , minus , multi, division, Modulo;
+    // sum -- niilber 
+    // minus -- hasah
+    // Multi(Multiplication)-- vrjih
+    // Division -- huvaah
+    // Modulo -- vldegdel avah
+    sum = a + b;
+    minus = a - b;
+    multi = a * b;
+    division = a / b;
+    Modulo = a%b;
+    printf ("%d\n%d\n%d\n%d\n%d", sum , minus , multi , division , Modulo);
+    return 0 ;
+}
